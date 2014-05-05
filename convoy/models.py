8e018affd73e7c667fd6588ad4240da774d45afd
Ted <ted.tieken@gmail.com>
2014-05-04 20:00:45 -0400

    Cleans up monkey patching

--- convoy/models.py
+++ convoy/models.py
@@ -1,27 +1,10 @@
-from django.db import models
-from django.http import HttpResponse
-import time
-import copy
-
-# orig_close = HttpResponse.close
-
-# def new_close(*args, **kwargs):
-#    print "monkey patched close"
-#    orig_close(*args, **kwargs)
-#    time.sleep(5)
-#    print "spent 5 seconds doing stuff without blocking the user"
-   
-# HttpResponse.close = new_close
-  
-
-from django.contrib.staticfiles.management.commands import collectstatic, runserver
-from django.core.management.base import CommandError
-from collections import OrderedDict
 from django.conf import settings
+from django.core.management.base import CommandError
+from django.contrib.staticfiles.management.commands import collectstatic, runserver
+from collections import OrderedDict
+
 
 orig_collect = collectstatic.Command.collect
-orig_run = runserver.Command.run
-
 def new_collect(self, *args, **kwargs):
     '''
     Monkey Patches collect so we have a clean OrderedDict each time the command is run    
@@ -32,15 +15,15 @@ def new_collect(self, *args, **kwargs):
     if hasattr(self.storage, "manifest_name"):
         self.storage.hashed_files = OrderedDict()
     return orig_collect(self, *args, **kwargs)
-
 collectstatic.Command.collect = new_collect
+
 
 runserver_error_message = """When CONVOY_DURING_DEBUG is set to True, you must
 * run runserver with the --nostatic option: $ python manage.py runserver --nostatic
 * run collectstatic each time your static files change
 * configure an explicit static serving url in your urls.py: e.g. url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT}'
 """
-
+orig_run = runserver.Command.run
 def new_run(self, *args, **kwargs):
     '''
     Monkey patches collectstatic's runserver to warn when a bad set of settigns are given
@@ -51,5 +34,16 @@ def new_run(self, *args, **kwargs):
         if kwargs.get('use_static_handler', True):
           raise CommandError(runserver_error_message)
     return orig_run(self, *args, **kwargs) 
-
 runserver.Command.run = new_run   
+
+
+# A monkey patch that might be able to let us do non-blocking post-request work
+#from django.http import HttpResponse
+#import time
+# orig_close = HttpResponse.close
+# def new_close(*args, **kwargs):
+#    print "monkey patched close"
+#    orig_close(*args, **kwargs)
+#    time.sleep(5)
+#    print "spent 5 seconds doing stuff without blocking the user"
+# HttpResponse.close = new_close
