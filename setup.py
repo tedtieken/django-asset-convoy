df2b1c656c3c014c27aae0e4de8370f63e4fc382
Peter Conerly <pconerly@gmail.com>
2014-05-09 11:40:09 -0700

    adding files for pypi

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from distutils.core import setup
+
+description = 'Asset packager for heroku/s3-hosted Django applications'
+try:
+   import pypandoc
+   long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+   long_description = description
+
+setup(
+    name='django-asset-convoy',
+    version='0.1.0',
+    author='Ted Tieken <ted.tieken@gmail.com>, Peter Conerly <pconerly@gmail.com>',
+    author_email='ted.tieken@gmail.com',
+    packages=['convoy'],
+    scripts=[],
+    url='',
+    license='license',
+    description=description,
+    long_description=long_description,
+    install_requires=[
+        "Django >= 1.7.0",
+        "django-s3-folder-storage >= 0.2",
+        "cssmin >= 0.2.0",
+        "rjsmin >= 1.0.9",
+    ],
+)
