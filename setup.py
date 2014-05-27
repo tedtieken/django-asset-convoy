b8f7d9c222f12dc5c8243223481b421be2ed6988
Peter Conerly <pconerly@gmail.com>
2014-05-27 16:32:42 -0700

    retain django 1.7

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     description=description,
     long_description=long_description,
     install_requires=[
-        # "Django >= 1.7.0",
+        "Django >= 1.7.0",
         # TODO update this when either: Django 1.7 is released or we have django 1.6 support
         "django-s3-folder-storage >= 0.2",
         "cssmin >= 0.2.0",
