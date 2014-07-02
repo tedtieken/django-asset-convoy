82033a63df68e281d7725e239220a613cf97297a
Peter Conerly <pconerly@gmail.com>
2014-07-02 16:08:37 -0700

    added another test

--- convoy/tests/tests.py
+++ convoy/tests/tests.py
@@ -5,6 +5,7 @@ from convoy.tests.adminscript import AdminScriptTestCase, test_dir
 class ManageRunserverEmptyAllowedHosts(AdminScriptTestCase):
     def setUp(self):
         print "setting up..."
+        # these are some simple defaults.
         self.write_settings('settings.py', 
             apps= [
                 'django.contrib.admin',
@@ -27,7 +28,6 @@ class ManageRunserverEmptyAllowedHosts(AdminScriptTestCase):
                 },
             }
         )
-        # this is not properly setting these things up
 
 
 
@@ -69,10 +69,43 @@ class ManageRunserverEmptyAllowedHosts(AdminScriptTestCase):
 
         out, err = self.run_manage(['collectstatic', '--noinput'])
 
-        # import ipdb
-        # ipdb.set_trace()
+        self.assertNoOutput(err)
+        print out
+        
+    def test_convoy_installedapp(self):
+        self.write_settings('settings.py', 
+            apps= [
+                'django.contrib.admin',
+                'django.contrib.auth',
+                'django.contrib.contenttypes',
+                'django.contrib.sessions',
+                'django.contrib.messages',
+                'django.contrib.staticfiles',
+                'convoy',
+            ],
+            sdict={
+                'ALLOWED_HOSTS': [],
+                'DEBUG': False,
+                'DATABASES': {
+                    'default': {
+                        'ENGINE': 'django.db.backends.sqlite3'
+                    },
+                    'other': {
+                        'ENGINE': 'django.db.backends.sqlite3',
+                    }
+                },
+                'STATIC_ROOT': "'static/'",
+                'STATIC_URL': "'/static/'",
+                'STATICFILES_STORAGE': "'convoy.stores.ConvoyStorage'",
+                'CONVOY_DURING_DEBUG': 'True',
+                'CONVOY_AWS_QUERYSTRING_AUTH': 'False',
+                'CARPOOL_COMBINE_DURING_DEBUG': 'True',
+            }
+        )
+        self.cat_settings('settings.py')
+
+        out, err = self.run_manage(['collectstatic', '--noinput'])
 
 
         self.assertNoOutput(err)
         print out
-        
